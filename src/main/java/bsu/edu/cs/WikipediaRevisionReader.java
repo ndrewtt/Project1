5e8d14cb085c412a282ef52b65293a1faa02cccf
ash8998 <napierfamily2001@gmail.com>
2025-09-30 20:35:24 -0400

    changed uri to url

--- src/main/java/bsu/edu/cs/WikipediaRevisionReader.java
+++ src/main/java/bsu/edu/cs/WikipediaRevisionReader.java
@@ -3,8 +3,8 @@ package bsu.edu.cs;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.URL;
+//import java.net.URLSyntaxException;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
@@ -28,14 +28,14 @@ public class WikipediaRevisionReader {
         String urlString = String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Zappa&rvprop=timestamp|user&rvlimit=4&redirects", articleTitle);
         String encodedUrlString = URLEncoder.encode(urlString, Charset.defaultCharset());
         try {
-            URI uri = new URI(encodedUrlString);
-            URLConnection connection = uri.toURL().openConnection();
+            URL url = new URL(encodedUrlString);
+            URLConnection connection = url.openConnection();
             connection.setRequestProperty("User-Agent", "WikipediaRevisionReader/0.1 (https://youtube.com/paulgestwicki; pvgestwicki@bsu.edu)");
             InputStream InputStream = connection.getInputStream();
             WikipediaRevisionParser parser = new WikipediaRevisionParser();
             String timestamp = parser.parse(InputStream);
             return timestamp;
-        } catch (MalformedURLException | URISyntaxException malformedURLException) {
+        } catch (MalformedURLException malformedURLException) {
             throw new RuntimeException(malformedURLException);
         }
     }
