16cea7317742ff690b100de4b159256f6cf0f892
ash8998 <napierfamily2001@gmail.com>
2025-10-03 19:36:34 -0400

    Updated build for GUI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -15,6 +17,11 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
     implementation("net.minidev:json-smart:2.6.0")
+}
+
+javafx {
+    modules("javafx.controls")
+    version = "25"
 }
 
 tasks.test {
